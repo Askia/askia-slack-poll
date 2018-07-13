79dbbc3bf82c92a3e2ad62c0e9292d24b5284116
Yoann <yoann@askia.com>
2018-07-13 16:23:36 +0200

    Restore token validation

--- index.js
+++ index.js
@@ -9,45 +9,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('port', (process.env.PORT || 9001));
 app.get('/', (req, res) => res.send('It works!'));
 
-app.post('/post', (req, res) => {
+app.post('/post', ({body: {token, response_url}}, res) => {
   res.status(200).end();
 
-  const {body, response_url, token} = req;
-
-  console.log(req);
-  sendMessageToSlackResponseURL(response_url, {
-    "text": "This is your first interactive message",
-    "attachments": [
-      {
-        "text": "Building buttons is easy right?",
-        "fallback": "Shame... buttons aren't supported in this land",
-        "callback_id": "button_tutorial",
-        "color": "#3AA3E3",
-        "attachment_type": "default",
-        "actions": [
-          {
-            "name": "yes",
-            "text": "yes",
-            "type": "button",
-            "value": "yes"
-          },
-          {
-            "name": "no",
-            "text": "no",
-            "type": "button",
-            "value": "no"
-          },
-          {
-            "name": "maybe",
-            "text": "maybe",
-            "type": "button",
-            "value": "maybe",
-            "style": "danger"
-          }
-        ]
-      }
-    ]
-  })
+  if (token !== process.env.SLACK_APP_TOKEN) {
+    console.error('Invalid token', token);
+    res.status(403).end('Access forbidden');
+  }
+  else {
+    sendMessageToSlackResponseURL(response_url, {
+      "text": "This is your first interactive message",
+      "attachments": [
+        {
+          "text": "Building buttons is easy right?",
+          "fallback": "Shame... buttons aren't supported in this land",
+          "callback_id": "button_tutorial",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "yes",
+              "text": "yes",
+              "type": "button",
+              "value": "yes"
+            },
+            {
+              "name": "no",
+              "text": "no",
+              "type": "button",
+              "value": "no"
+            },
+            {
+              "name": "maybe",
+              "text": "maybe",
+              "type": "button",
+              "value": "maybe",
+              "style": "danger"
+            }
+          ]
+        }
+      ]
+    })
+  }
 });
 
 app.listen(app.get('port'), () => {
