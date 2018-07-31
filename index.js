80111524ee8f01c46ad7f1d6d1cd4744f383f4b4
Yoann <yoann@askia.com>
2018-07-31 15:00:50 +0200

    Log `message_ts`

--- index.js
+++ index.js
@@ -109,6 +109,8 @@ app.post(
 
       response.votes += 1;
 
+      console.log("payload::message_ts", payload.message_ts);
+      
       web.chat.delete({
         "channel": poll.channelId,
         "ts"     : payload.message_ts
